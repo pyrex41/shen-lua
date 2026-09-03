465af0764e4143849d2f446aed3054a650ec6b79
pyrex41 <reub.brooks@gmail.com>
2026-09-03 18:46:37 -0500

    nix: package and test shen-lua

--- flake.nix
+++ flake.nix
@@ -1,8 +1,59 @@
 {
-  description = "shen-lua development environment";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { nixpkgs, ... }: let systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ]; each = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system}); in {
-    packages = each (pkgs: { toolchain = pkgs.buildEnv { name = "shen-lua-toolchain"; paths = with pkgs; [ gnumake gcc luajit luarocks ]; }; default = pkgs.buildEnv { name = "shen-lua-toolchain"; paths = with pkgs; [ gnumake gcc luajit luarocks ]; }; });
-    devShells = each (pkgs: { default = pkgs.mkShell { packages = with pkgs; [ gnumake gcc luajit luarocks ]; }; });
-  };
+  description = "Shen 42 on LuaJIT";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      eachSystem = f: nixpkgs.lib.genAttrs systems (system: f system (import nixpkgs { inherit system; }));
+    in {
+      packages = eachSystem (_system: pkgs: rec {
+        shen-lua = pkgs.stdenvNoCC.mkDerivation {
+          pname = "shen-lua";
+          version = "0.11.0-dev";
+          src = self;
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          nativeCheckInputs = [ pkgs.luajit ];
+          dontBuild = true;
+          doCheck = true;
+          checkPhase = ''
+            runHook preCheck
+            export HOME="$TMPDIR"
+            make test LUA=${pkgs.luajit}/bin/luajit
+            runHook postCheck
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p "$out/share/shen-lua" "$out/bin"
+            cp -R . "$out/share/shen-lua/"
+            makeWrapper ${pkgs.luajit}/bin/luajit "$out/bin/shen" \
+              --add-flags "$out/share/shen-lua/bin/shen"
+            runHook postInstall
+          '';
+        };
+
+        default = shen-lua;
+      });
+
+      apps = eachSystem (system: _pkgs: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/shen";
+          meta.description = "Run Shen 42 on LuaJIT";
+        };
+      });
+
+      checks = eachSystem (system: _pkgs: {
+        default = self.packages.${system}.default;
+      });
+
+      devShells = eachSystem (_system: pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.luajit pkgs.gnumake ];
+        };
+      });
+    };
 }
